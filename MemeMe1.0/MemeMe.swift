5a2a599e81d2fea9b39f1f2a2a8d840c56632368
AdhikariM <maheshadhikari@live.com>
2024-02-11 22:02:59 -0600

    initial commit MemeMe

---
+++ MemeMe1.0/MemeMe.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMe.swift
+//  MemeMe1.0
+//
+//  Created by Mahesh Adhikari on 01/22/2024.
+
+import Foundation
+import UIKit
+
+//a file that contains the Meme structure
+struct MemeMe {
+    var topTextField = "TOP"
+    var bottomTextField = "BOTTOM"
+    var ogImage = UIImage()
+    var memeImage = UIImage()
+}
+
